4f20e14c27c69db765e1bc29697bf27e316fe5f2
EhsanTgv <EhsanTgv@gmail.com>
2020-12-26 23:37:07 +0330

    setup simple music player

--- app/src/main/java/com/taghavi/twoplayers/MainActivity.kt
+++ app/src/main/java/com/taghavi/twoplayers/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.taghavi.twoplayers
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupMusicPlayer()
+    }
+
+    private fun setupMusicPlayer() {
+        mediaPlayer = MediaPlayer()
+        try {
+            mediaPlayer.setDataSource(this, Uri.parse("http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3"))
+            mediaPlayer.prepareAsync()
+            mediaPlayer.setOnPreparedListener {
+                Log.i("media", "started")
+                mediaPlayer.start()
+            }
+        } catch (e: Exception) {
+            Log.i("media", "$e")
+            e.printStackTrace()
+        }
     }
 }
